4705844fa34cb6a50db5df74f3c01fa6e6551b30
Guilherme <guilherme-serrao.dev@outlook.pt>
2020-01-31 23:03:07 +0000

    Moving

---
+++ Assets/Scripts/GroundChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundChecker : MonoBehaviour
+{
+    public bool ground;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            ground = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            ground = false;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float horizontalSpeed;
+
+    [SerializeField]
+    private float verticalAddSpeed;
+
+    
+    private float airSpeed;
+
+    private Rigidbody2D rigidBody;
+    private bool moving;
+    private bool flying;
+
+    private bool grounded;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        grounded = transform.GetChild(0).GetComponent<GroundChecker>().ground;
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidBody.velocity = new Vector2(horizontalSpeed, rigidBody.velocity.y);
+            moving = true;
+           // transform.rotation =
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidBody.velocity = new Vector2(-horizontalSpeed, rigidBody.velocity.y);
+            moving = true;
+        }
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            flying = true;
+            rigidBody.velocity = new Vector2(rigidBody.velocity.x, airSpeed);
+        }
+
+        if (flying)
+        {
+            airSpeed += verticalAddSpeed;
+        }
+        else if (!flying)
+        {
+            airSpeed = 0;
+        }
+
+        /* else if (!moving)
+         {
+             rigidBody.velocity = new Vector2(0, rigidBody.velocity.y);
+         }*/
+
+        flying = false;
+        moving = false;
+
+        
+        
+    }
+
+}
